7ff7b8cf97eaf17fa8f54b1d7d88bf22cc442165
Juan Gutiérrez <juanesguti19@gmail.com>
2022-08-04 21:21:35 -0500

    files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Dialog } from '@capacitor/dialog'; 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'capApp';
+
+  async ngOnInit(): Promise<void> {
+  }
+
+  async showAlert() {
+    await Dialog.alert({
+      title: 'Atención',
+      message: 'Esto es una alertaaa!!'
+    });
+  }
 }
